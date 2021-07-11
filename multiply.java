db47430bedbe5fb1e681a67301da6af4a401fc0b
Deepanshu Chaudhary <officialteotia@gmail.com>
2021-07-11 17:36:21 +0530

    modified mult. code

--- multiply.java
+++ multiply.java
@@ -5,7 +5,7 @@ public class MultiplicationOfTwoNumbers {
       System.out.println("Enter the value of the first number ::");
       int a = sc.nextInt();
       System.out.println("Enter the value of the first number ::");
-      int b = sc.nextInt();
+      int b = sc.nextInt();  
       int result = a*b;
       System.out.println("Product of the given two numbers is ::"+result);
    }
